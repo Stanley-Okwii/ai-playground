375e0d585c1a875ae3cd332fea9beb620bf375c4
StanleeO <stanlee.okwii@gmail.com>
2023-07-23 21:54:42 +0300

    feat(header): add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export const Header = () => (
+  <div className="sm:mx-auto sm:w-full pb-10">
+    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+      GENERATIVE AI PLAYGROUND
+    </h2>
+    <p className="font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+      Unleash your imagination as you interact with state-of-the-art AI models,
+      and generate text. Dive into a vast repository of prompt ideas tailored to
+      various domains, from customer feedback to legal contracts, propelling you
+      towards successful AI applications.
+    </p>
+  </div>
+);
