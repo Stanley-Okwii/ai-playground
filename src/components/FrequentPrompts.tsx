1c86722ca856c949e985fa5ba3032952112c5813
StanleeO <stanlee.okwii@gmail.com>
2023-07-23 21:53:18 +0300

    feat(frequent-prompts): add FrequentPrompts component

---
+++ src/components/FrequentPrompts.tsx
@@ -0,0 +1,41 @@
+import { data } from "../data";
+
+import { useEffect, useState } from "react";
+
+interface FrequentPromptProps {
+  setInputValue: (value: string) => void;
+  sendRequest: () => void;
+}
+
+export const FrequentPrompts = ({
+  setInputValue,
+  sendRequest,
+}: FrequentPromptProps) => {
+  const [text, setText] = useState<string>("");
+  const { prompts } = data;
+
+  useEffect(() => {
+    if (text) {
+      setTimeout(() => {
+        sendRequest();
+      }, 500);
+    }
+  }, [text]);
+
+  return (
+    <>
+      {prompts.map(({ id, text }) => (
+        <button
+          key={id}
+          onClick={() => {
+            setInputValue(text);
+            setText(text);
+          }}
+          className="block hover:cursor-pointer max-w-sm p-3 mb-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <p className="font-normal text-gray-700 dark:text-gray-400">{text}</p>
+        </button>
+      ))}
+    </>
+  );
+};
